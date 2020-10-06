908d6d2f9290d531d521eeb34b8fd5590c9357f0
b sheats <sheatsb@users.noreply.github.com>
2020-10-06 03:45:44 -0400

    fix for webpack uglify

--- src/app.js
+++ src/app.js
@@ -1,5 +1,1 @@
 import './style.css'
-
-document.addEventListener('DOMContentLoaded', () => {
-  // JavaScript (ES7+) code here
-})
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
